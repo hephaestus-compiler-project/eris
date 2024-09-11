ed079712e8389307d2c165e9ca0ad43788593d66
Thodoris Sotiropoulos <theosotr@aueb.gr>
2024-09-11 15:22:31 +0200

    [translators]: add support for Scala loops

--- src/translators/scala.py
+++ src/translators/scala.py
@@ -1023,3 +1023,22 @@ class ScalaTranslator(BaseTranslator):
         )
         self._children_res.append(res)
         return res
+
+    @append_to
+    def visit_loop(self, node):
+        children = node.children()
+        for c in children:
+            c.accept(self)
+        children_res = self.pop_children_res(children)
+        loop_prefix = (
+            "while (true)"
+            if node.loop_type == ast.Loop.WHILE_LOOP
+            else "for (n <- Seq())"
+        )
+        res = "{ident}{prefix}{body}".format(
+            ident=self.get_ident(old_ident=self.ident),
+            prefix=loop_prefix,
+            body=children_res[0]
+        )
+        self._children_res.append(res)
+        return res
