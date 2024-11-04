f5e7efc173ee994ff5ade48b3de4bf1afa756da7
Thodoris Sotiropoulos <theosotr@aueb.gr>
2024-11-04 07:17:07 +0100

    Improve type abstractions

--- src/enumerators/type_abstractions.py
+++ src/enumerators/type_abstractions.py
@@ -38,6 +38,8 @@ TypeAbstraction = Union[TypeParameterAbstraction, SimpleClassifierAbstraction,
 def get_inheritance_tree(t: tp.Type, bt_factory: bt.BuiltinFactory) -> str:
     tree = nx.DiGraph()
     tree.add_node(t.name)
+    nx.set_node_attributes(tree, {t.name: get_type_sign(t)},
+                           name="type")
     visited = set()
     visited.add(t.name)
     worklist = []
@@ -51,6 +53,8 @@ def get_inheritance_tree(t: tp.Type, bt_factory: bt.BuiltinFactory) -> str:
         for st in supertypes:
             if st not in tree:
                 tree.add_node(st.name)
+                nx.set_node_attributes(tree, {st.name: get_type_sign(st)},
+                                       name="type")
                 tree.add_edge(st.name, n.name)
                 worklist.append(st)
     return tree
@@ -62,7 +66,7 @@ def get_type_sign(t: tp.Type) -> str:
     elif t.is_type_constructor():
         return "TC"
     elif t.is_parameterized():
-        return "PT"
+        return "PT" + str(len(t.type_args))
     elif t.is_wildcard():
         return "T"
     else:
@@ -75,11 +79,12 @@ def to_type_abstraction(t: tp.Type,
         return TypeParameterAbstraction(t.bound is not None)
 
     tree_hash = nx.weisfeiler_lehman_graph_hash(get_inheritance_tree(
-        t, bt_factory))
+        t, bt_factory), node_attr="type")
 
     if t.is_parameterized():
-        signs = [get_type_sign(type_arg) for type_arg in t.type_args]
-        return ParameterizedTypeAbstraction(".".join(signs), tree_hash)
+        signs = [to_type_abstraction(type_arg,
+                                     bt_factory) for type_arg in t.type_args]
+        return ParameterizedTypeAbstraction(tuple(signs), tree_hash)
 
     if t.is_type_constructor():
         type_params = [
